f05cdc51dbc55aa919faca6671f6c5d6f82a8d87
Alex Varela <shion093@gmail.com>
2018-07-19 10:07:14 -0600

    testing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,38 @@
-Promise = require('bluebird');
-const { port, env } = require('./config/constants');
-const mongoose = require('./config/mongoose');
-const app = require('./config/express');
+// Promise = require('bluebird');
+// const { port, env } = require('./config/constants');
+// const mongoose = require('./config/mongoose');
+// const app = require('./config/express');
+//
+// mongoose.connect();
+//
+//
+// app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+//
+// module.exports = app;
+const path = require('path');
+const { NodeMediaCluster } = require('node-media-server');
+const numCPUs = require('os').cpus().length;
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  },
+  https: {
+    port: 8443,
+    key: path.join(__dirname, 'privatekey.pem'),
+    cert:path.join(__dirname,'certificate.pem'),
+  },
+  cluster: {
+    num: numCPUs
+  }
+};
 
-mongoose.connect();
-
-
-app.listen(port, () => console.info(`server started on port ${port} (${env})`));
-
-module.exports = app;
+var nmcs = new NodeMediaCluster(config)
+nmcs.run();
