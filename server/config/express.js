8be68317e58d63eb62103af7f8b4e91abb9849ff
Alex Varela <shion093@gmail.com>
2018-07-18 18:21:59 -0600

    media

--- server/config/express.js
+++ server/config/express.js
@@ -11,6 +11,42 @@ const { handler, converter, notFound } = require('../middlewares/errors');
 
 const app = express();
 
+const MediaServer = require('medooze-media-server');
+
+const ip = '127.0.0.1';
+
+//Create UDP server endpoint
+const endpoint = MediaServer.createEndpoint(ip);
+
+console.log(endpoint);
+
+const SemanticSDP	= require('semantic-sdp');
+const SDPInfo		= SemanticSDP.SDPInfo;
+const MediaInfo		= SemanticSDP.MediaInfo;
+const CandidateInfo	= SemanticSDP.CandidateInfo;
+const DTLSInfo		= SemanticSDP.DTLSInfo;
+const ICEInfo		= SemanticSDP.ICEInfo;
+const StreamInfo	= SemanticSDP.StreamInfo;
+const TrackInfo		= SemanticSDP.TrackInfo;
+const Direction		= SemanticSDP.Direction;
+const CodecInfo		= SemanticSDP.CodecInfo;
+
+//Create new streamer
+const streamer = MediaServer.createStreamer();
+
+//Create new video session codecs
+const video = new MediaInfo("video","video");
+
+//Add h264 codec
+video.addCodec(new CodecInfo("h264",96));
+
+//Create session for video
+const session = streamer.createSession(video, {
+  local  : {
+    port: 5004
+  }
+});
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -49,11 +85,117 @@ io.on('connection', (socket) => {
     users: [socket.id]
   });
 
-  socket.on('make-offer', function(data) {
-    console.log('made', data);
-    socket.to(data.to).emit('offer-made', {
-      offer: data.offer,
-      socket: socket.id
+  socket.on('make-offer', function(frame) {
+    console.log(frame);
+    var data = JSON.parse(frame);
+    var offer = SDPInfo.process(data.offer);
+
+    console.log('gtgtg', offer);
+
+    //Create an DTLS ICE transport in that enpoint
+    const transport = endpoint.createTransport({
+      dtls : offer.getDTLS(),
+      ice  : offer.getICE()
+    });
+
+    console.log('ferferfe', transport)
+
+    //Set RTP remote properties
+    transport.setRemoteProperties({
+      audio : offer.getMedia("audio"),
+      video : offer.getMedia("video")
+    });
+
+    //Get local DTLS and ICE info
+    const dtls = transport.getLocalDTLSInfo();
+    const ice  = transport.getLocalICEInfo();
+
+
+    //Get local candidates
+    const candidates = endpoint.getLocalCandidates();
+
+    console.log('234234234', candidates)
+
+    //Create local SDP info
+    let answer = new SDPInfo();
+
+    //Add ice and dtls info
+    answer.setDTLS(dtls);
+    answer.setICE(ice);
+    //For each local candidate
+    for (let i=0;i<candidates.length;++i)
+      //Add candidate to media info
+      answer.addCandidate(candidates[i]);
+
+    //Get remote video m-line info
+    let audioOffer = offer.getMedia("audio");
+
+    console.log('dasdwdww', audioOffer)
+
+    //If offer had video
+    if (audioOffer)
+    {
+      //Create video media
+      let  audio = new MediaInfo(audioOffer.getId(), "audio");
+      //Set recv only
+      audio.setDirection(Direction.INACTIVE);
+      //Add it to answer
+      answer.addMedia(audio);
+    }
+
+    //Get remote video m-line info
+    let videoOffer = offer.getMedia("video");
+
+
+    console.log('dasdwdww', videoOffer)
+    //If offer had video
+    if (videoOffer)
+    {
+      //Create video media
+      let  video = new MediaInfo(videoOffer.getId(), "video");
+
+      console.log('videooo', video)
+
+      //Get codec types
+      let h264 = videoOffer.getCodec("h264");
+      //Add video codecs
+      video.addCodec(h264);
+      //Set recv only
+      video.setDirection(Direction.RECVONLY);
+      //Add it to answer
+      answer.addMedia(video);
+    }
+
+    //Set RTP local  properties
+    transport.setLocalProperties({
+      audio : answer.getMedia("audio"),
+      video : answer.getMedia("video")
+    });
+
+
+    //Create new local stream with only video
+    const outgoingStream  = transport.createOutgoingStream({
+      audio: false,
+      video: true
+    });
+
+
+    console.log('rfuryfur', outgoingStream)
+    console.log('rfuryfur333', outgoingStream.getVideoTracks());
+
+    //Copy incoming data from the broadcast stream to the local one
+    outgoingStream.getVideoTracks()[0].attachTo(session.getIncomingStreamTrack());
+
+    //Get local stream info
+    const info = outgoingStream.getStreamInfo();
+
+    //Add local stream info it to the answer
+    answer.addStream(info);
+
+
+    io.to(socket.id).emit('answer-made', {
+      socket: socket.id,
+      answer: answer,
     });
   });
 
@@ -67,7 +209,7 @@ io.on('connection', (socket) => {
 
   socket.on('make-answer', function(data) {
     console.log(data);
-    io.to(data.to).emit('answer-made', {
+    io.to(data.to).emit('answer-made2', {
       socket: socket.id,
       answer: data.answer
     });
